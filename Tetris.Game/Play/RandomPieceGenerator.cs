da42f63e26db32dc5854ec93dd04d10e453e4a96
ojh050118 <ojh0050118@gmail.com>
2022-06-26 00:55:50 +0900

    Add PieceRandomGenerator based on 7-bag system

---
+++ Tetris.Game/Play/RandomPieceGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using osu.Framework.Extensions.IEnumerableExtensions;
+using osu.Framework.Utils;
+using Tetris.Game.Pieces;
+
+namespace Tetris.Game.Play
+{
+    public class RandomPieceGenerator
+    {
+        public Queue<PieceType> Bag { get; }
+
+        public Queue<PieceType> PendingBag { get; }
+
+        public RandomPieceGenerator()
+        {
+            Bag = new Queue<PieceType>();
+            PendingBag = new Queue<PieceType>();
+            fill();
+        }
+
+        public PieceType NextPiece()
+        {
+            if (Bag.Count == 0)
+                fill();
+
+            return Bag.Dequeue();
+        }
+
+        private void fill()
+        {
+            while (PendingBag.Count > 0)
+                Bag.Enqueue(PendingBag.Dequeue());
+
+            shuffle().ForEach(pieceType => PendingBag.Enqueue(pieceType));
+
+            if (Bag.Count == 0)
+                fill();
+        }
+
+        private PieceType[] shuffle()
+        {
+            List<PieceType> bag = new List<PieceType>();
+            Func<PieceType> nextPiece = () => (PieceType)RNG.Next(0, 7);
+
+            int count = 0;
+
+            while (count < 7)
+            {
+                var pieceType = nextPiece.Invoke();
+
+                if (bag.Contains(pieceType))
+                    continue;
+
+                bag.Add(pieceType);
+                count++;
+            }
+
+            return bag.ToArray();
+        }
+    }
+}
