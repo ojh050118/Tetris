f17d18dffe714330d2c36b4bfea9f6dc01e1b3fb
ojh050118 <ojh0050118@gmail.com>
2022-07-24 01:17:43 +0900

    Design main menu

--- Tetris.Game/Screens/MainScreen.cs
+++ Tetris.Game/Screens/MainScreen.cs
@@ -1,30 +1,207 @@
+using System;
+using System.Linq;
 using osu.Framework.Allocation;
+using osu.Framework.Extensions.Color4Extensions;
 using osu.Framework.Graphics;
+using osu.Framework.Graphics.Colour;
+using osu.Framework.Graphics.Containers;
 using osu.Framework.Graphics.Shapes;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.Textures;
+using osu.Framework.Input.Events;
 using osu.Framework.Screens;
+using osu.Framework.Utils;
+using osuTK;
 using osuTK.Graphics;
-using Tetris.Game.Screens.Play;
 
 namespace Tetris.Game.Screens
 {
     public class MainScreen : Screen
     {
+        private BufferedContainer background;
+
         [BackgroundDependencyLoader]
-        private void load()
+        private void load(LargeTextureStore textures, TetrisGameBase game)
         {
             InternalChildren = new Drawable[]
             {
-                new Box
+                background = new BufferedContainer
                 {
-                    Colour = Color4.Violet,
+                    RedrawOnScale = false,
                     RelativeSizeAxes = Axes.Both,
+                    Children = new Drawable[]
+                    {
+                        new Sprite
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Texture = textures.Get("bg1.jfif"),
+                            FillMode = FillMode.Fill
+                        }
+                    }
                 },
-                new Stage
+                new Container
                 {
-                    Anchor = Anchor.Centre,
-                    Origin = Anchor.Centre,
+                    RelativeSizeAxes = Axes.Both,
+                    Width = 0.5f,
+                    Masking = true,
+                    Shear = new Vector2(0.2f, 0),
+                    Children = new Drawable[]
+                    {
+                        new BufferedContainer
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Masking = true,
+                            BlurSigma = new Vector2(7),
+                            Children = new Drawable[]
+                            {
+                                background.CreateView().With(d =>
+                                {
+                                    d.RelativeSizeAxes = Axes.Both;
+                                    d.SynchronisedDrawQuad = true;
+                                    d.DisplayOriginalEffects = true;
+                                })
+                            }
+                        },
+                        new FillFlowContainer
+                        {
+                            RelativeSizeAxes = Axes.Both,
+                            Direction = FillDirection.Vertical,
+                            Spacing = new Vector2(0, 10),
+                            Children = new Drawable[]
+                            {
+                                new TextButton("Play"),
+                                new TextButton("Settings"),
+                                new TextButton("Exit")
+                                {
+                                    Action = game.RequestExit
+                                }
+                            }
+                        }
+                    }
                 }
             };
+        }
+
+        private class TextButton : ClickableContainer
+        {
+            private readonly string text;
+            private Box hover;
+
+            private const int wind_effect_amount = 5;
+
+            public TextButton(string text)
+            {
+                this.text = text;
+                Anchor = Anchor.Centre;
+                Origin = Anchor.Centre;
+                RelativeSizeAxes = Axes.X;
+                AutoSizeAxes = Axes.Y;
+            }
+
+            [BackgroundDependencyLoader]
+            private void load()
+            {
+                Children = new Drawable[]
+                {
+                    new SpriteText
+                    {
+                        Anchor = Anchor.Centre,
+                        Origin = Anchor.Centre,
+                        Text = text,
+                        Font = FontUsage.Default.With(size: 28),
+                        Margin = new MarginPadding(4),
+                        Shadow = true,
+                        ShadowColour = Color4.Black.Opacity(0.5f)
+                    },
+                    hover = new Box
+                    {
+                        RelativeSizeAxes = Axes.Both,
+                        Width = 0.8f,
+                        Colour = ColourInfo.GradientHorizontal(Color4.White.Opacity(0.4f), Color4.Transparent)
+                    }
+                };
+            }
+
+            protected override bool OnHover(HoverEvent e)
+            {
+                hover.FadeIn(100, Easing.OutPow10);
+
+                for (int count = 0; count < wind_effect_amount; count++)
+                {
+                    var wind = createWind(RNG.NextSingle(0.5f));
+
+                    Add(wind);
+                    wind.FadeOut().MoveToY(RNG.NextSingle(-14, 14)).FadeIn().MoveToX(RNG.NextSingle(200, 450), RNG.NextSingle(1000, 1500), Easing.OutPow10).FadeOut(RNG.NextSingle(2000, 3000), Easing.OutPow10).Expire();
+                }
+
+                return base.OnHover(e);
+            }
+
+            protected override void OnHoverLost(HoverLostEvent e)
+            {
+                base.OnHoverLost(e);
+
+                foreach (var d in Array.FindAll(Children.ToArray(), h => h.GetType() != typeof(SpriteText)))
+                    d.FadeOut(700, Easing.Out);
+            }
+
+            protected override bool OnClick(ClickEvent e)
+            {
+                Box box;
+
+                Add(box = new Box
+                {
+                    Anchor = Anchor.TopRight,
+                    Origin = Anchor.TopRight,
+                    RelativeSizeAxes = Axes.Both,
+                    Width = 0.8f,
+                    Colour = ColourInfo.GradientHorizontal(Color4.Transparent, Color4.White.Opacity(0.4f))
+                });
+                box.FadeOut(1000, Easing.OutPow10).Expire();
+                hover.FlashColour(Color4.Transparent, 1000, Easing.OutPow10);
+
+                return base.OnClick(e);
+            }
+
+            private Drawable createWind(float width = 0.5f)
+            {
+                return new GridContainer
+                {
+                    Anchor = Anchor.CentreLeft,
+                    Origin = Anchor.CentreRight,
+                    RelativeSizeAxes = Axes.X,
+                    AutoSizeAxes = Axes.Y,
+                    Width = width,
+                    Depth = 1,
+                    RowDimensions = new[]
+                    {
+                        new Dimension(GridSizeMode.AutoSize)
+                    },
+                    Content = new[]
+                    {
+                        new Drawable[]
+                        {
+                            new Box
+                            {
+                                RelativeSizeAxes = Axes.X,
+                                Height = 2,
+                                Colour = ColourInfo.GradientHorizontal(Color4.Transparent, Color4.White)
+                            },
+                            new Box
+                            {
+                                RelativeSizeAxes = Axes.X,
+                                Height = 2,
+                            },
+                            new Box
+                            {
+                                RelativeSizeAxes = Axes.X,
+                                Height = 2,
+                                Colour = ColourInfo.GradientHorizontal(Color4.White, Color4.Transparent)
+                            }
+                        }
+                    }
+                };
+            }
         }
     }
 }
--- Tetris.Game/TetrisGameBase.cs
+++ Tetris.Game/TetrisGameBase.cs
@@ -1,6 +1,7 @@
 using osu.Framework.Allocation;
 using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Textures;
 using osu.Framework.IO.Stores;
 using Tetris.Game.Input;
 using Tetris.Resources;
@@ -11,6 +12,11 @@ namespace Tetris.Game
     {
         protected override Container<Drawable> Content { get; }
 
+        private DependencyContainer dependencies;
+
+        protected override IReadOnlyDependencyContainer CreateChildDependencies(IReadOnlyDependencyContainer parent) =>
+            dependencies = new DependencyContainer(base.CreateChildDependencies(parent));
+
         protected TetrisGameBase()
         {
             base.Content.Add(Content = new DrawSizePreservingFillContainer());
@@ -19,7 +25,13 @@ namespace Tetris.Game
         [BackgroundDependencyLoader]
         private void load()
         {
+            var largeStore = new LargeTextureStore(Host.CreateTextureLoaderStore(new NamespacedResourceStore<byte[]>(Resources, @"Textures")));
+
             Resources.AddStore(new DllResourceStore(typeof(TetrisResources).Assembly));
+
+            dependencies.CacheAs(largeStore);
+            dependencies.CacheAs(this);
+
             base.Content.Add(new TetrisKeyBindingContainer(this));
         }
     }
